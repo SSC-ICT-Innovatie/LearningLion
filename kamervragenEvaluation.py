2c3de8fec153ec6b0803afb6e993c3c4b9ea4245
Mark Heijnekamp <struiken_klieven.0u@icloud.com>
2024-10-03 16:33:21 +0200

    Refactor: Rearrange question and context concatenation logic

--- kamervragenEvaluation.py
+++ kamervragenEvaluation.py
@@ -392,7 +392,11 @@ def evaluate_with_sample_questions(samplequestionsCSVPATH,querier: Querier, toCS
         df = pd.read_csv(samplequestionsCSVPATH)
         correct_count = 0
         for index, row in df.iterrows():
-            question = f"{row['context']} {row['Question']}"
+            question = ""
+            if(addContext):
+                question = f"{row['Question']} {row['context']}"
+            else:
+                question = f"{row['Question']}"
             documents = querier.get_documents_with_scores(question=question)
             if len(documents) > 0:
                 if documents[0][0].metadata['filename'] == row['Filename']:
